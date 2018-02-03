50b45a0875c83367de6a9b63b1ad856949a2575f
Chris Clower <chris@clowerweb.com>
2018-02-03 00:37:54 -0500

    Let's not set routes in index

--- App/routes.php
+++ App/routes.php
@@ -1,12 +1,30 @@
 <?php
 
-$router = new Core\Router();
+declare(strict_types = 1);
 
-// index
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-// route generic controller/action stuff
-$router->add('{controller}', ['action' => 'index']);
-$router->add('{controller}/{action}');
-$router->add('{controller}/{action}/{token:[\da-f]+}');
+namespace App;
 
-$router->dispatch($_SERVER['QUERY_STRING']);
+/**
+ * Routes class
+ *
+ * PHP version 7.0
+ */
+class Routes {
+	/**
+	 * Add routes to the application
+	 *
+	 * @param object $router - the router class
+	 *
+	 * @return void
+	 */
+	public static function addRoutes(object $router) : void {
+		// index
+		$router->add('', ['controller' => 'Home', 'action' => 'index']);
+		// route generic controller/action stuff
+		$router->add('{controller}', ['action' => 'index']);
+		$router->add('{controller}/{action}');
+		$router->add('{controller}/{action}/{token:[\da-f]+}');
+
+		$router->dispatch($_SERVER['QUERY_STRING']);
+	}
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use App\Routes;
+
 /**
  * Default time zone to use application-wide. It is HIGHLY RECOMMENDED that you LEAVE THIS AS 'UTC', because it is
  * the universal time, even if you are located in a different time zone. Several utility functions require times to
@@ -25,4 +27,6 @@ set_exception_handler('Core\Error::exceptionHandler');
 /**
  * Routing
  */
-require_once dirname(__DIR__) . '/App/routes.php';
+$router = new Core\Router();
+
+Routes::addRoutes($router);
