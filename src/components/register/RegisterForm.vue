48dfd387b644b167f44b5b4e47f9863c90166fe1
Chris Clower <chris@clowerweb.com>
2020-02-09 14:51:02 -0500

    Move registration form to Vue component, styles, build

---
+++ src/components/register/RegisterForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <h3 class="page-title">Register</h3>
+
+        <div v-if="errors" class="errors alert alert-error">
+            <strong>Please correct the following error(s):</strong>
+
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </div>
+
+        <p>
+            <strong>Already have an account? <a href="/login">Login</a></strong>
+        </p>
+
+        <form method="post" action="/register" id="form-register">
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Email" :value="email" required />
+            </div>
+
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" placeholder="Password" required />
+            </div>
+            <div id="password-error-container"></div>
+
+            <button type="submit" class="btn btn-primary">Sign up</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'register-form',
+        props: ['email', 'errors']
+    };
+</script>
