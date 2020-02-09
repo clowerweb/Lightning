8a88b06a25b443fcb97f59e8801cd9612831aff9
Chris Clower <chris@clowerweb.com>
2020-02-09 14:35:01 -0500

    Move forgot password form to Vue component

---
+++ src/components/password/ForgotForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <h3 class="page-title">Password reset request</h3>
+
+        <form method="post" action="/password/request-reset">
+            <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" id="email" class="form-control" name="email" placeholder="Email address" autofocus required />
+            </div>
+
+            <button type="submit" class="btn btn-primary">Send password reset</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'forgot-form'
+    };
+</script>
