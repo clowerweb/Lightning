c878f059ce8fc9300ca614aabdaabecd4a77b0eb
Chris Clower <chris@clowerweb.com>
2020-02-10 22:22:11 -0500

    Move flash messages to Vue component

---
+++ src/components/global/FlashMessages.vue
@@ -0,0 +1,53 @@
+<template>
+    <section v-if="messages" class="container">
+        <div
+            v-for="message in messages"
+            class="alert"
+            :class="`alert-${message.type}`"
+            role="alert"
+        >
+            {{ message.body }}
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'flash-messages',
+        props: ['messages']
+    }
+</script>
+
+<style scoped lang="scss">
+    .alert {
+
+        border: rem(1) solid transparent;
+        margin-bottom: rem(20);
+        padding: rem(12) rem(20);
+
+        &-success {
+            background-color: $color-green-l;
+            border-color: $color-green;
+            color: $color-green;
+        }
+
+        &-info {
+            background-color: $color-blue-l;
+            border-color: $color-blue;
+            color: $color-blue;
+        }
+
+        &-warning {
+            background-color: $color-orange-l;
+            border-color: $color-orange;
+            color: $color-orange;
+        }
+
+        &-error {
+            background-color: $color-red-l;
+            border-color: $color-red;
+            color: $color-red;
+        }
+
+    }
+</style>
